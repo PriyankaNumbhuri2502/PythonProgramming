d58fccbe8e04c9c9e3a0888dcc0125fee27a6195
Priyanka Numbhuri <57513293+PriyankaNumbhuri2502@users.noreply.github.com>
2022-02-22 13:17:41 +0530

    Update powerOfTwo.py

--- powerOfTwo.py
+++ powerOfTwo.py
@@ -1,7 +1,31 @@
+# Using Recursion
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n == 0:
+            return False
+        if n == 1:
+            return True
+        if n % 2 == 0:
+            return self.isPowerOfTwo(int(n/2))
+        return False
+
+
+
+#Using Bitwise Manipulation
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
         return n > 0 and not(n & n -1)
-      
+   
+
+'''
+Runtime     Memory
+38 ms       13.8 MB
+45 ms       13.9 MB
+
+'''
+
+
+
 '''
 s = Solution()
 print(s.isPowerOfTwo(12))
@@ -13,3 +37,4 @@ False
 True
 
 '''
+
